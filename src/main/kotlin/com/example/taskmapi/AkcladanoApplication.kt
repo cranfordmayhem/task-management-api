8e23f11290df78e760ada1d3ebcd0eee4b5230f9
Avel Kristian Ladaño <akladano@AvelMBA-M1-017.local>
2025-12-03 08:15:12 +0800

    initial commit

---
+++ src/main/kotlin/com/example/taskmapi/AkcladanoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.taskmapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AkcladanoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AkcladanoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/taskmapi/AkcladanoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.taskmapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AkcladanoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
